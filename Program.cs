51e6b642b6f3637e5ffb23731f41f913608a26b6
krist <krist@LAPTOP-8UDCB928>
2021-03-19 13:17:33 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DistSysAcwClient
+{
+    class Program
+    {
+
+        private static string request = "";
+
+        public static string ApiKey { get; set; }
+        public static string username { get; set; }
+
+       
+        static void Initialise()
+        {
+           
+
+            Console.WriteLine("Hello. What would you like to do?");
+            while (true)
+            {
+                request = Console.ReadLine();
+                Console.Clear();
+                clientViewModel.RunRequest(request).Wait();
+                Console.WriteLine("Hello. What would you like to do?");
+            }
+        }
+
+        
+        static void Main(string[] args)
+        {
+            clientViewModel clientViewModel = new clientViewModel();
+            Initialise();
+            
+        }
+    }
+}
---
+++ clientViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace DistSysAcwClient
+{
+    class clientViewModel
+    {
+        private static HttpClient mClient = new HttpClient();
+        private const string mUrl = "https://localhost:44394/api/";
+
+        public clientViewModel()
+        {
+            // default constructor 
+            RunAsync().Wait();
+
+        }
+
+        public static async Task RunRequest(string request)
+        {
+            if (!string.IsNullOrEmpty(request))
+            {
+                string[] args = request.Split(' ');
+
+                if (args.Length == 2)
+                    {
+                        // get the values of the strings and make request
+                        string requestValues = args[0] + " " + args[1];
+                        var splitedValue = "";
+                        splitedValue = args[1];
+
+
+                        switch (requestValues)
+                        {
+                            case "TalkBack Hello":
+                                Hello(splitedValue).Wait();
+                                break;
+                        }
+                    }
+           
+                if (args.Length == 3)
+                {
+                    var endpoint = "integers=";
+                        // get the values of the strings and make request
+                        string requestValues = args[0] + " " + args[1] + " " + args[2];
+                        if (requestValues.Contains("Sort") && requestValues.Contains("TalkBack"))
+                        {
+
+                            string[] parsed = args[2].Split(',');
+
+                            int i = 0;
+                            foreach (var integer in parsed)
+                            {
+                                string trimmedValue = integer.Trim(' ', '[', ']', ',');
+                                
+                                if (i++ == 0)
+                                {
+                                    endpoint += trimmedValue;
+                                }
+                                else
+                                {
+                                    endpoint += "&integers=" + trimmedValue;
+                                }
+
+                            }
+                            Sort(endpoint).Wait();
+                    }
+                      
+
+                }
+
+            }
+
+        }
+
+
+        static async Task Hello(string args)
+       {
+           var response = mClient.GetAsync("talkback/" + args);
+           var result = await response.Result.Content.ReadAsStringAsync();
+           Console.WriteLine(result);
+       }
+
+       static async Task Sort(string args)
+       {
+           var endpoint = mClient.GetAsync("talkback/Sort?" + args);
+           var response = await endpoint.Result.Content.ReadAsStringAsync();
+           Console.WriteLine(response);
+       }
+
+        static async Task RunAsync()
+       {
+           mClient.BaseAddress = new Uri(mUrl);
+       }
+
+    }
+}
